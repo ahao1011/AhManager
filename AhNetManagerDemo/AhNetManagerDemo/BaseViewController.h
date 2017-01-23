23fdf65e3d1e4bc5d4a2b21ae9163a8a1e1a0c37
ah <zth1011@126.com>
2017-01-23 17:58:43 +0800

    添加弹框类

---
+++ AhNetManagerDemo/AhNetManagerDemo/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  AhNetManagerDemo
+//
+//  Created by ah on 2017/1/23.
+//  Copyright © 2017年 ah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
