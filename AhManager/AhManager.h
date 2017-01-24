89414a12d68c6fd744ae4d4e910844e478ae3a2f
ah <zth1011@126.com>
2017-01-24 11:08:21 +0800

    增加AhManager.h文件

---
+++ AhManager/AhManager.h
@@ -0,0 +1,18 @@
+//
+//  AhManager.h
+//  AhNetManagerDemo
+//
+//  Created by ah on 2017/1/24.
+//  Copyright © 2017年 ah. All rights reserved.
+//
+
+#ifndef AhManager_h
+#define AhManager_h
+
+#import "AhRequest.h"
+#import "AhResult.h"
+#import "NetManager.h"
+#import "SystemSet.h"
+#import "NetTool.h"
+
+#endif /* AhManager_h */
---
+++ AhNetManagerDemo/AhNetManagerDemo/AhManager/AhManager.h
@@ -0,0 +1,13 @@
+//
+//  AhManager.h
+//  AhNetManagerDemo
+//
+//  Created by ah on 2017/1/24.
+//  Copyright © 2017年 ah. All rights reserved.
+//
+
+#ifndef AhManager_h
+#define AhManager_h
+
+
+#endif /* AhManager_h */
